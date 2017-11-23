cba12a7ab07d06dd0efbfcbd39621d1bfd238168
Polo Duf <paulhenri.dufourcq@gmail.com>
2017-11-23 15:51:57 +0100

    add the dose model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,3 @@
 class Cocktail < ApplicationRecord
   validates :name, uniqueness: true, presence: true, inclusion: { in: ["Mint Julep", "Whiskey Sour", "Mojito"], allow_nil: false}
-
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,3 @@
 class Ingredient < ApplicationRecord
+  validates :name, uniqueness: true, presence: true, :description, presence: true, inclusion: { in: ["lemon", "ice", "mint leaves"], allow_nil: false}
 end
